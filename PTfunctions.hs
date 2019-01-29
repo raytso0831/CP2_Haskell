687377b2a0e9a51b8b5cd2cfd2a6332fedc6f129
raTsyto31 Hhs <ratsyto31@hanovernorwichschools.org>
2019-01-29 15:36:37 +0000

    Homework

---
+++ PTfunctions.hs
@@ -0,0 +1,18 @@
+-- module PTfunctions
+quadraticVertex a b c = (-b)/ 2*a
+quadraticDiscriminant a b c = b*b - 4*a*c
+
+quadraticRealSolutionQty a b c = if quadraticDiscriminant a b c< 0
+                        then 0
+                        
+                        else if quadraticDiscriminant a b c == 0
+                        then 1
+                        
+                        else 2
+                       
+convertFtoC x = (x-32) / 5/9
+convertCtoF x = ( x*9/5 ) + 32
+convertTemp x u = if u == "f"
+                    then convertFtoC x
+                    else convertCtoF x
+
